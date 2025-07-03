8145f9fa0ed7879d381853554b083a291ce01422
cloudred <cloudred442@gmail.com>
2025-07-03 13:24:27 +0800

    fix callback

--- apps/nextjs/app/(protected)/dashboard/page.tsx
+++ apps/nextjs/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { generatePageMetadata } from "@/app/metadata";
+
+export async function generateMetadata() {
+  return generatePageMetadata({
+    title: "Dashboard | CoverGenie",
+    description: "Dashboard | CoverGenie",
+  });
+}
+
 const DashboardPage = () => {
   return <div>DashboardPage</div>;
 };
--- apps/nextjs/app/(public)/sign-in/[[...sign-in]]/page.tsx
+++ apps/nextjs/app/(public)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,12 @@
+import { generatePageMetadata } from "@/app/metadata";
 import SignInPage from "@/components/SignInPage/SignInPage";
+
+export async function generateMetadata() {
+  return generatePageMetadata({
+    title: "Sign In | CoverGenie",
+    description: "Sign In | CoverGenie",
+  });
+}
 
 const page = () => {
   return <SignInPage />;
