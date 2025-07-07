c77dae1ea57670f9419817b5316b8f2b97ad614b
cloudred <cloudred442@gmail.com>
2025-07-07 18:47:31 +0800

    add workspace

--- apps/nestjs/src/api/v1/user/user.controller.ts
+++ apps/nestjs/src/api/v1/user/user.controller.ts
@@ -7,6 +7,17 @@ import { UserService } from "./user.service";
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Post("/get-user-data")
+  async getUserData(@Req() req: FastifyRequestWithUser) {
+    try {
+      const user = req.user;
+
+      return await this.userService.getUserData(user);
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
   @Post("onboarding")
   async create(@Body() createUserOnboardingDto: UserOnboardingDto, @Req() req: FastifyRequestWithUser) {
     try {
--- apps/nestjs/src/api/v1/user/user.service.ts
+++ apps/nestjs/src/api/v1/user/user.service.ts
@@ -10,6 +10,31 @@ export class UserService {
     @Inject("ClerkClient") private readonly clerk: ClerkClient,
   ) {}
 
+  async getUserData(user: User) {
+    const userData = await this.prisma.user_table.findUnique({
+      where: { user_id: user.id },
+      include: {
+        workspace_member_table: {
+          include: {
+            roles: true,
+            workspace: true,
+          },
+        },
+      },
+    });
+
+    const formattedUserData = {
+      user_id: userData?.user_id,
+      email: userData?.email,
+      first_name: userData?.first_name,
+      last_name: userData?.last_name,
+      workspace_member_table: userData?.workspace_member_table,
+      workspace: userData?.workspace_member_table?.[0]?.workspace,
+    };
+
+    return formattedUserData;
+  }
+
   async createOnboarding(createUserOnboardingDto: UserOnboardingDto, user: User) {
     await this.prisma.$transaction(async (tx) => {
       await tx.user_onboarding_table.create({
